69280e66a0c0fcfa3e08ad88b4b7a9bad0537247
Chris (from laptop/git desktop app) <38088640+chuynh18@users.noreply.github.com>
2018-06-28 01:08:43 -0700

    realized the navbar component won't be helpful.  moved navbar component into App.js due to how React handles data propagation.  Otherwise i'd have to build the "body" of my app as a child of the navbar component anyway

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,76 @@
 import React, { Component } from "react";
 import "./App.css";
 import API from "./utils/API";
-import Navbar from "./components/Navbar";
 
 class App extends Component {
 
-  scrapeHome() {
-    API.scrapeHome()
-    .then(res => {
-      console.log(res);
-    })
-  };
+    state = {
+        value: ""
+    };
 
-  scrapeSection(section) {
-    API.scrapeSection(section)
-    .then (res => {
-      console.log(res);
-    })
-  };
+    scrapeHome() {
+        API.scrapeHome()
+        .then(res => {
+            console.log(res);
+        })
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <Navbar
-          home = {this.scrapeHome}
-          section = {this.scrapeSection}
-          />
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    scrapeSection(section) {
+        API.scrapeSection(section)
+        .then (res => {
+            console.log(res);
+        })
+    };
+
+    handleChange = event => {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if (this.state.value === "") {
+            this.scrapeHome();
+        }
+        else {
+            this.scrapeSection(this.state.value);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label><span>NYT Section: </span>
+                        <span><select value={this.state.value} onChange={this.handleChange}>
+                            <option value="">Home page</option>
+                            <option value="world">World</option>
+                            <option value="us">U.S.</option>
+                            <option value="politics">Politics</option>
+                            <option value="nyregion">N.Y.</option>
+                            <option value="business">Business</option>
+                            <option value="opinion">Opinion</option>
+                            <option value="technology">Tech</option>
+                            <option value="science">Science</option>
+                            <option value="health">Health</option>
+                            <option value="sports">Sports</option>
+                            <option value="arts">Arts</option>
+                            <option value="books">Books</option>
+                            <option value="fashion">Style</option>
+                            <option value="food">Food</option>
+                            <option value="travel">Travel</option>
+                            <option value="magazine">Magazine</option>
+                            <option value="t-magazine">T Magazine</option>
+                            <option value="realestate">Real Estate</option>
+                            <option value="obituaries">Obituaries</option>
+                            <option value="learning">The Learning Network</option>
+                            <option value="multimedia">Multimedia</option>
+                        </select></span>
+                    </label>
+                    <span> <input type="submit" value="Scrape" /></span>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -26,35 +26,37 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label><span>NYT Section: </span>
-                    <span><select value={this.state.value} onChange={this.handleChange}>
-                        <option value="">Home page</option>
-                        <option value="world">World</option>
-                        <option value="us">U.S.</option>
-                        <option value="politics">Politics</option>
-                        <option value="nyregion">N.Y.</option>
-                        <option value="business">Business</option>
-                        <option value="opinion">Opinion</option>
-                        <option value="technology">Tech</option>
-                        <option value="science">Science</option>
-                        <option value="health">Health</option>
-                        <option value="sports">Sports</option>
-                        <option value="arts">Arts</option>
-                        <option value="books">Books</option>
-                        <option value="fashion">Style</option>
-                        <option value="food">Food</option>
-                        <option value="travel">Travel</option>
-                        <option value="magazine">Magazine</option>
-                        <option value="t-magazine">T Magazine</option>
-                        <option value="realestate">Real Estate</option>
-                        <option value="obituaries">Obituaries</option>
-                        <option value="learning">The Learning Network</option>
-                        <option value="multimedia">Multimedia</option>
-                    </select></span>
-                </label>
-                <span> <input type="submit" value="Scrape" /></span>
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label><span>NYT Section: </span>
+                        <span><select value={this.state.value} onChange={this.handleChange}>
+                            <option value="">Home page</option>
+                            <option value="world">World</option>
+                            <option value="us">U.S.</option>
+                            <option value="politics">Politics</option>
+                            <option value="nyregion">N.Y.</option>
+                            <option value="business">Business</option>
+                            <option value="opinion">Opinion</option>
+                            <option value="technology">Tech</option>
+                            <option value="science">Science</option>
+                            <option value="health">Health</option>
+                            <option value="sports">Sports</option>
+                            <option value="arts">Arts</option>
+                            <option value="books">Books</option>
+                            <option value="fashion">Style</option>
+                            <option value="food">Food</option>
+                            <option value="travel">Travel</option>
+                            <option value="magazine">Magazine</option>
+                            <option value="t-magazine">T Magazine</option>
+                            <option value="realestate">Real Estate</option>
+                            <option value="obituaries">Obituaries</option>
+                            <option value="learning">The Learning Network</option>
+                            <option value="multimedia">Multimedia</option>
+                        </select></span>
+                    </label>
+                    <span> <input type="submit" value="Scrape" /></span>
+                </form>
+            </div>
         );
     }
 }
